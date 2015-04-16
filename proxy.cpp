d51bfcd3073ac8f47fbc64764acdc60e165ce8e4
Brandon Chao <brandonchao3@gmail.com>
2015-04-16 03:33:38 -0400

    More initial scaffolding

--- proxy.cpp
+++ proxy.cpp
@@ -13,18 +13,25 @@ using namespace std;
 
 
 /* Global Variables */
-int serverPort;
-int cacheSize
+const int MAX_BACK_LOG = 5;
+int server_port;
+int cache_size
 
 /* Function Prototypes */
 
-void* requestThread();
-void handleBrowserRequests(int client_sock);
-
+void* request_thread();
+void handle_browser_requests(int client_sock);
+string get_host_response(string request);
+string get_message(int sock, bool host);
+void send_message(int sock, string message);
 
 
 int main (int argc, char* argv[])
 {
+
+  int sock, client_sock;
+  struct sockaddr_in server_addr, client_addr;
+
   // Check to see if correct number of inputs
   if (argc != 2) {
     cerr << "Wrong number of arguments." << endl;
@@ -32,63 +39,89 @@ int main (int argc, char* argv[])
   }
 
   // Save server port and cache size as global variables
-  serverPort = atoi(argv[1]);
-  cacheSize = atoi(argv[2]);
+  server_port = atoi(argv[1]);
+  cache_size = atoi(argv[2]);
 
   // Create the socket connection
-  int sock;
-  if (sock = socket(AF_INET, SOCK_STREAM, 0) < 0) {
+  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
     cerr << "Error creating socket." << endl;
     exit(-1);
   }
 
   // Set the fields in the socket
-    // TODO: Look in Snowcast server
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(server_port);
 
   // Bind the server port to the socket
-    // TODO: Look in Snowcast server 
+  if (bind(sock, (struct sockaddr*) &server_addr, sizeof(server_addr)) < 0) {
+    cerr << "Error binding socket." << endl;
+    exit(-1);
+  }
 
-  // Listen on socket
-    // TODO: Look in Snowcast server
+  // Listen on the socket
+  listen(sock, MAX_BACK_LOG);
 
   // Wait for connections
   while (true) {
 
     // Accept the connection
+    socklen_t len = sizeof(client_addr);
+    if ((client_sock = accept(sock, (struct sockaddr*) &client_addr, &len)) < 0) {
+      cerr << "Error accepting socket." << endl;
+      continue;
+    }
 
     // Use pthread_create to make new thread to and call requestThread
-      // Remember to pass in the client_sock into requestThread
+    pthread_t client_thread;
+
+    if ((pthread_create(&client_thread, NULL, request_thread, (void*) &client_sock) != 0)) {
+      cerr << "Error creating child process thread." << endl;
+      close(client_sock);
+      pthread_exit(NULL);
+    }
 
   }
 
   return 0;
+
 }
 
-void* requestThread() {
+void* request_thread(void* client_sock_value) {
 
-  int client_sock;
+  int client_sock = *((int*) client_sock_value);
+
+  // Do I need to detach the thread here???
 
   handleBrowserRequests(client_sock);
 
   close(client_sock);
 
   pthread_exit(NULL);
+
 }
 
-void handleBrowserRequests(int client_sock) {
+void handle_browser_requests(int client_sock) {
 
+  string request, response;
 
   // Get request from browser
+  request = get_message(client_sock, false);
 
   // Check if cache contains request, if so grab corresponding response
+  response = check_cache(request);
 
   // If not, then connect with host to get response
+  if (response == "") {
+    response = get_host_response(request);
+  }
 
   // Send the message response back to the client
+  send_message(client_sock, response);
 
 }
 
-string connectWithHost(string requestMessage) {
+string get_host_response(string request) {
 
   // Extract host IP address from message
 
@@ -99,5 +132,13 @@ string connectWithHost(string requestMessage) {
   // Forward the request message to host
 
   // Get the response from host and return
+
+}
+
+string get_message(int sock, bool host) {
+
+}
+
+void send_message(int sock, string message) {
 
 }
