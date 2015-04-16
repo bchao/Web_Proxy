8a4cf0666c89ba50cd6b8d8e4d47eb978cc5ce80
yomikaze <ypeter999@gmail.com>
2015-04-16 01:01:04 -0400

    get from command line

---
+++ proxy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int port, cacheSize;
+  cout << "Port: ";
+  cin >> port;
+  cout << "Cache size: ";
+  cin >> cacheSize;
+  cout << "You entered port " << port << "\n";
+  cout << "You entered cache size " << cacheSize << "\n";
+  return 0;
+ }
